4840b365574de82c714fd0a73d2c7103bf625c25
Milman <milindmjoshi@live.com>
2024-02-29 21:47:40 -0500

    Get Circle and Square working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Circle = require('./lib/shapes.cjs');
+//const Circle = require('./lib/shapes.cjs');
+const Circle = require('./lib/circle.cjs');
+const Square = require('./lib/square.cjs');
+const Shape = require ('./lib/shapes.cjs');
 
 inquirer
     .prompt([
@@ -29,10 +32,27 @@ inquirer
     ])
     .then((answers)=>{
         console.log(answers);
-        var circle = new Circle(answers.color,answers.logoText,answers.logoTextColor);
-        console.log(circle);
-        fs.writeFile("logo.svg", circle.render(),(err) =>
-           err ? console.error(err) : console.log('Generated logo.svg'));
+        var shape;
+        switch (answers.shape){
+            case 'Circle':
+                 shape = new Circle(answers.color,answers.logoText,answers.logoTextColor); 
+                 break;
+            case 'Square':
+                shape = new Square(answers.color,answers.logoText,answers.logoTextColor);
+                break;
+            case 'Triangle':
+                shape = new Shape(answers.color,answers.logoText,answers.logoTextColor);
+                break;
+        }
+        // var circle = new Circle(answers.color,answers.logoText,answers.logoTextColor);
+        // var square = new Square(answers.color,answers.logoText,answers.logoTextColor);
+        //console.log(circle);
+        // fs.writeFile("logo.svg", circle.render(),(err) =>
+        //    err ? console.error(err) : console.log('Generated logo.svg'));
+        // fs.writeFile("logo.svg", square.render(),(err) =>
+        //    err ? console.error(err) : console.log('Generated logo.svg'));
+        fs.writeFile("logo.svg", shape.render(),(err) =>
+            err ? console.error(err) : console.log('Generated logo.svg'));
     })
     .catch((error)=>{
         console.log(error);
