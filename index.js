a37a3565dc09931ae18f36210f22b498f3c9f796
Milman <milindmjoshi@live.com>
2024-02-29 21:24:28 -0500

    Get Circle SVG generation working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/shapes.cjs');
 
 inquirer
     .prompt([
@@ -27,7 +29,10 @@ inquirer
     ])
     .then((answers)=>{
         console.log(answers);
-        console.log("Gnerated logo.svg");
+        var circle = new Circle(answers.color,answers.logoText,answers.logoTextColor);
+        console.log(circle);
+        fs.writeFile("logo.svg", circle.render(),(err) =>
+           err ? console.error(err) : console.log('Generated logo.svg'));
     })
     .catch((error)=>{
         console.log(error);
