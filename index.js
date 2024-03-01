7573f7269352f3910f78b5b9b2e57c12eaf862bd
Milman <milindmjoshi@live.com>
2024-02-29 20:24:02 -0500

    Get basic prompts working

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 const inquirer = require('inquirer');
 
 inquirer
-    .createPromptModule([
+    .prompt([
         {
-            type: list,
-            name: shape,
+            type: 'list',
+            name: 'shape',
             message: 'Please select a logo shape',
             choices: ['Circle','Square','Triangle'],
             default: 0
         },
         {
-            type: input,
-            name: color,
+            type: 'input',
+            name: 'color',
             message: 'Please enter logo color',
         },
         {
-            type: input,
-            name: text,
+            type: 'input',
+            name: 'logoText',
             message: 'Please enter logo text',
         },
         {
-            type: input,
-            name: text,
+            type: 'input',
+            name: 'logoTextColor',
             message: 'Please enter logo text color',
-        },
-
-
-
-
-
+        }
     ])
     .then((answers)=>{
         console.log(answers);
+        console.log("Gnerated logo.svg");
     })
     .catch((error)=>{
         console.log(error);
